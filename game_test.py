c11425d74223598c2b60a63c1bcbf59de651751c
Jatin Gupta <Jatin Gupta>
2016-11-13 22:04:00 -0500

    initial commit

---
+++ game_test.py
@@ -0,0 +1,131 @@
+# _*_ coding:utf-8 _*_
+import operator
+import sys
+import wikipedia
+import nltk
+from nltk import sent_tokenize,word_tokenize
+import string
+import pygame
+
+pygame.init()
+#to take the input from the command line
+search_item = sys.argv[1]
+print(search_item)
+
+summaryWiki = wikipedia.summary("Wikipedia")
+
+results = wikipedia.search(search_item,5,False)
+    
+result_dict = {} 
+for item in results:
+    result_dict[item] = wikipedia.page(item).content
+
+text_list = []
+for item in result_dict.values():
+    text_list.append(word_tokenize(item))
+        
+taggedList = []    
+for item in text_list:
+    taggedList.append(nltk.pos_tag(item))
+    
+adjList = []
+for listItem in taggedList:
+    for innerListItem in listItem:
+        if innerListItem[1]=='JJ':
+            adjList.append(innerListItem[0].lower())
+
+freqDict = {}
+for item in adjList:
+    if item in freqDict:
+        freqDict[item] +=1
+    else:
+        freqDict[item] = 1     
+        
+sortedFreq = sorted(freqDict.items(), key=operator.itemgetter(1))
+
+topAdjList = []
+for i in range(len(sortedFreq)-6,len(sortedFreq)):
+    topAdjList.append(sortedFreq[i][0]) 
+    
+print(topAdjList)
+adj_List = topAdjList
+
+
+pygame.display.set_caption("Game of adjectives")
+
+black = 0, 0, 0
+white = 255,255,255
+red = 255, 0, 0
+green = 0,255,0
+
+width = 800
+height = 600
+
+
+screen = pygame.display.set_mode((width,height))
+clock = pygame.time.Clock()
+
+def text_objects(text,font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
+    
+def drawBall(x,y,color,text):
+    pygame.draw.circle(screen,color,(x,y),50)
+    smallText = pygame.font.Font("freesansbold.ttf",20)
+    textSurf, textRect = text_objects(text, smallText)
+    textRect.center = (x,y)
+    screen.blit(textSurf, textRect)
+    
+def displayKeyPressed(key):
+    text = 'keys typed :' + key
+    bottomText = pygame.font.Font("freesansbold.ttf",30)
+    textSurf, textRect = text_objects(text, bottomText)
+    textRect.center = (400,500)
+    screen.blit(textSurf, textRect)
+    
+def displayGameOver():
+    largeText = pygame.font.Font("freesansbold.ttf",60)
+    textSurf, textRect = text_objects('GAME OVER!!', largeText)
+    textRect.center = (400,300)
+    screen.blit(textSurf, textRect)
+    
+def game_loop():
+    crashed = False
+    start_x = 50
+    start_y = 50
+    change = 1
+    str_keyPressed = ''
+    adjIndex = []
+    while not crashed:
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: 
+                crashed = True
+            if event.type == pygame.KEYDOWN and len(pygame.key.name(event.key))==1:
+                key_name = pygame.key.name(event.key)
+                str_keyPressed += key_name
+                for item in adj_List:
+                    if item[0] == key_name:
+                        if adj_List.index(item) not in adjIndex:
+                            adjIndex.append(adj_List.index(item))
+                            item= ''
+        
+        start_y += change
+        screen.fill(white)
+        displayKeyPressed(str_keyPressed)
+        
+        for i in range(6):
+            if i not in adjIndex:
+                drawBall(start_x+(50+i*100), start_y, (200+i,255-(i*50),(i**3)),adj_List[i])
+                
+            if len(adjIndex)==6: 
+                screen.fill(white)
+                displayGameOver()                
+        
+        pygame.display.update()
+        
+        clock.tick(15)
+    
+game_loop()
+pygame.quit()
+quit()
